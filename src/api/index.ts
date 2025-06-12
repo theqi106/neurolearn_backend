7896d9c52da7517a0bf67e233082f88b51cd2ea1
theqi106 <phkhhulolmb1@gmail.com>
2025-06-12 14:55:11 +0700

    feat: add payment method

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,6 +9,7 @@ import levelRoutes from '../routes/level.route';
 import quizRoutes from '../routes/quiz.route';
 import progressRoutes from '../routes/progress.route';
 import incomeRoutes from '../routes/income.route';
+import paymentRoutes from '../routes/payment.route';
 
 const router = express.Router();
 
@@ -31,5 +32,7 @@ router.use('/quizzes', quizRoutes);
 router.use('/progress', progressRoutes);
 
 router.use('/income', incomeRoutes);
+
+router.use('/payment', paymentRoutes);
 
 export default router;
